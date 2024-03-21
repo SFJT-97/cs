31845f05a325595f18010cd68e46faf11340b12f
SFJT-97 <santiagojuarez.dev@gmail.com>
2024-03-21 10:23:25 -0400

    Array!

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/* using System;
 
 namespace HelloWorld
 {
@@ -8,4 +8,29 @@ namespace HelloWorld
             Console.WriteLine("Hello World! I'm just getting started");
         }
     }
+} */
+
+using System;
+
+namespace Array
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+        string[] words1 = new string[3];
+
+        words1[0] = "Hello ";
+        words1[1] = "I'm ";
+        words1[2] = "getting ";
+
+        string[] words2 = {"World! ", "just ", "started. "};
+
+        string phrase = words1[0] + words2[0] + words1[1] + words2[1] + words1[2] + words2[2];
+
+        Console.WriteLine(phrase);
+
+    }
+  }
 }
--- ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c3e8f8190973f240f0ee18a9125db99fc54e203")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
